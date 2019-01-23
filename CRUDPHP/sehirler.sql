2f6d879335d309058ea033742c6ca5289f00fe88
AnilTural <46935121+AnilTural@users.noreply.github.com>
2019-01-23 16:50:58 +0300

    Add files via upload

---
+++ CRUDPHP/sehirler.sql
@@ -0,0 +1,39 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Anamakine: 127.0.0.1
+-- Üretim Zamanı: 23 Oca 2019, 13:14:15
+-- Sunucu sürümü: 5.6.17
+-- PHP Sürümü: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Veritabanı: `sehirler`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `sehir`
+--
+
+CREATE TABLE IF NOT EXISTS `sehir` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `isim` varchar(50) NOT NULL,
+  `telefonkodu` int(11) NOT NULL,
+  `plakakodu` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
